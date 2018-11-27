300c7f1b9bf13934d8e7ee8c23ac95cad37fc97b
Bruno Carvalho <brunocarvalho287@gmail.com>
2018-11-27 14:30:07 -0300

    First commit

---
+++ src/main/java/br/ufc/quixada/encartecinema/EncarteCinemaApplication.java
@@ -0,0 +1,12 @@
+package br.ufc.quixada.encartecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncarteCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncarteCinemaApplication.class, args);
+	}
+}
---
+++ src/main/java/br/ufc/quixada/encartecinema/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.ufc.quixada.encartecinema;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EncarteCinemaApplication.class);
+	}
+
+}
